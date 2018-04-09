e80cc052bb4faf1ebb197861430339a7cd071a1f
yangzhenghao <1129195141@qq.com>
2018-04-10 00:33:55 +0800

    Add files via upload

--- code/SemanticAnalysis.h
+++ code/SemanticAnalysis.h
@@ -11,4 +11,8 @@
 //	the interface to call code_genaration
 //		input_Tree	: the syntax tree
 //		output_File : the filename without suffix
-void Code_Generate(Programstruct* input_Tree, string output_File);
+void semantic_Error_Detect(Programstruct* input_Tree, string output_File);
+
+bool lookup_symbol(string symbol_sheet, string symbol_name);
+string get_symbol_type(string symbol_sheet, string symbol_name);
+vector<pair<int, int> > get_symbol_range(string symbol_sheet, string symbol_name);
